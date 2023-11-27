597b1835c18f7882d41430b2d9c889967a527d44
boc1234 <joey_chusana@hotmail.com>
2023-11-27 17:01:07 +0700

    new in develop

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xff1D1E33);
+const kBottomContainerColor = Color(0xffEB1555);
+const kInactiveCardColor = Color(0xff111328);
+
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
