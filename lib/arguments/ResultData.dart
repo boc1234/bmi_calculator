1a03ad12c073f6681ccf9125222542cb773a6376
boc1234 <joey_chusana@hotmail.com>
2024-01-05 10:37:50 +0700

    add arguments

---
+++ lib/arguments/ResultData.dart
@@ -0,0 +1,7 @@
+class ResultDate{
+  late String bmiResult;
+  late String resulText;
+  late String interpretation;
+  ResultDate({required this.bmiResult,required this.interpretation,required this.resulText});
+
+}
